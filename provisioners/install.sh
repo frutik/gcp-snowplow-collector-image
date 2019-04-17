d37073f8350a791a3a582aa862cc81bde47d13e8
Andrew Kornilov <frutik@gmail.com>
2019-04-17 22:06:12 +0200

    Update install.sh

--- provisioners/install.sh
+++ provisioners/install.sh
@@ -12,6 +12,9 @@ apt-get -y install unzip
 mkdir $DIR
 cd $DIR
 
+mv /tmp/.env $DIR/.env
+mv /tmp/config.hocon $DIR/config.hocon
+
 wget https://dl.bintray.com/snowplow/snowplow-generic/snowplow_scala_stream_collector_google_pubsub_${VERSION}.zip
 unzip snowplow_scala_stream_collector_google_pubsub_${VERSION}.zip
 
